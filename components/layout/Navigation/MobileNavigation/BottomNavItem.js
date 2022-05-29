fb5d6b38961722350b1fbfc63b77442a29df38d8
lanojalal <lanodler95@gmail.com>
2022-05-29 23:44:38 +0200

    new color fix nav

--- components/layout/Navigation/MobileNavigation/BottomNavItem.js
+++ components/layout/Navigation/MobileNavigation/BottomNavItem.js
@@ -7,13 +7,23 @@ const BottomNavItem = ({ path, icon, name }) => {
     <div
       onClick={() => router.push(path)}
       className={` ${
-        router.pathname == path
-          ? "text-red-400 hover:text-red-400"
-          : "hover:text-gray-200"
+        router.pathname == path ? "text-red-400 hover:text-red-400" : ""
       } px-2 flex flex-col items-center font-semibold tracking-wider transition cursor-pointer text-white`}
     >
-      <span className="px-1 material-symbols-rounded">{icon}</span>
-      <h1 className={`text-xs`}>{name}</h1>
+      <span
+        className={` ${
+          router.pathname == path ? " text-red-400" : "text-white"
+        }  px-1   material-symbols-rounded `}
+      >
+        {icon}
+      </span>
+      <h1
+        className={`${
+          router.pathname == path ? " text-red-400" : "text-white"
+        } text-xs`}
+      >
+        {name}
+      </h1>
     </div>
   );
 };
--- components/layout/Navigation/TopNavbar/NavbarItem.js
+++ components/layout/Navigation/TopNavbar/NavbarItem.js
@@ -7,12 +7,20 @@ const NavbarItem = ({ path, icon, name }) => {
     <div
       onClick={() => router.push(path)}
       className={` ${
-        router.pathname == path ? " text-red-400 " : " hover:text-gray-200"
-      } px-2 flex  items-center font-semibold tracking-wider transition cursor-pointer text-white`}
+        router.pathname == path ? "" : " "
+      } px-2 flex  items-center font-semibold tracking-wider transition cursor-pointer `}
     >
-      <span className="px-1 text-4xl  material-symbols-rounded ">{icon}</span>
+      <span
+        className={` ${
+          router.pathname == path ? " text-red-400" : "text-white"
+        } hover:text-gray-200 px-1 text-4xl  material-symbols-rounded `}
+      >
+        {icon}
+      </span>
       <h1
-        className={` ${router.pathname == path ? "block " : "hidden"} text-sm `}
+        className={` ${
+          router.pathname == path ? "block text-red-400" : "hidden"
+        } text-sm `}
       >
         {name}
       </h1>
